6c882c5870b2c7db08e7dc764cb944d8220566b3
yunseokjeong <wjddbstjr03@naver.com>
2023-11-16 12:49:08 +0900

    initial commit

---
+++ simulator/simulator.py
@@ -0,0 +1,50 @@
+
+class Simulator:
+    
+    def __init__(self) -> None:
+        self.source_code
+        self.hardware = {1: led}
+        
+    def digitalread(pin: int):
+        
+    def userinteraction(code):
+        codelookup = -> related hw, 한일
+        그 친구 찾아서 실행
+        다른 애들 스테이트 업데이트
+    
+
+class Board:
+    def __init__(self) -> None:
+        self.pin1
+        self.pin2
+        ...
+        
+        self.gnd
+        self.vcc
+        
+        
+class LED:
+    def __init__(self) -> None:
+        self.left
+        self.right
+    pass   
+class Button:
+    def __init__(self) -> None:
+        self.left
+        self.right
+    pass   
+class TemperatureSensor:
+    def __init__(self) -> None:
+        self.left
+        self.right
+    pass   
+class Potentialmeter:
+    def __init__(self) -> None:
+        self.left
+        self.right
+    pass   
+class Photoresistor:
+    def __init__(self) -> None:
+        self.left
+        self.right
+    pass    
