706f9c3d1009e7bc7c2d4b97df8853ad17b31432
Nayoung-Oh <lightsalt540@gmail.com>
2023-11-21 02:47:34 +0900

    really simple one only with direct connection with board

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,50 +1,110 @@
-
-class Simulator:
-    
-    def __init__(self) -> None:
-        self.source_code
-        self.hardware = {1: led}
-        
-    def digitalread(pin: int):
-        
-    def userinteraction(code):
-        codelookup = -> related hw, 한일
-        그 친구 찾아서 실행
-        다른 애들 스테이트 업데이트
-    
+class Pin:
+    def __init__(self, name="pin"):
+        self.state = 0
+        self.name = name
+        self.connect = []
+    def __str__(self) -> str:
+        return self.name
 
 class Board:
-    def __init__(self) -> None:
-        self.pin1
-        self.pin2
-        ...
+    def __init__(self):
+        self.pin1 = Pin("pin1")
+        self.pin2 = Pin("pin2")
+        self.gnd = []
+        self.vcc = Pin("vcc")
+        self.vcc.state = 1
         
-        self.gnd
-        self.vcc
+    def __str__(self) -> str:
+        return "board"
+
+    def digitalread(self, pin: int):
+        if pin == 1:
+            return self.pin1.state
+        elif pin == 2:
+            return self.pin2.state
+    
+    def digitalwirte(self, pin: int, value: int):
+        if pin == 1:
+            self.pin1.state = value
+        elif pin == 2:
+            self.pin2.state = value
+        self.updatecircuit()
         
+    def userinteraction(self, code):
+        board.code[code]()
+        self.updatecircuit()
+
+    def updatecircuit(self):
+        # starting from gnd?
+        pass
         
-class LED:
-    def __init__(self) -> None:
-        self.left
-        self.right
-    pass   
-class Button:
-    def __init__(self) -> None:
-        self.left
-        self.right
-    pass   
-class TemperatureSensor:
-    def __init__(self) -> None:
-        self.left
-        self.right
-    pass   
-class Potentialmeter:
-    def __init__(self) -> None:
-        self.left
-        self.right
-    pass   
-class Photoresistor:
-    def __init__(self) -> None:
-        self.left
-        self.right
-    pass    
+class Component:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+        # asume flow left to right
+        # thus use left as its identifier, right to find next component
+    
+class LED(Component):
+    def __init__(self, left, right):
+        # assume left is - right is +
+        super().__init__(left, right) 
+    def __str__(self) -> str:
+        return "LED with right {}".format(self.right)
+
+class Button(Component):
+    def __init__(self, left, right):
+        super().__init__(left, right) 
+        # if you push it, the state becomes high
+    def press(self):
+        self.right.state = 1
+    def unpress(self):
+        self.right.state = 0
+    def __str__(self) -> str:
+        return "BTN with right {}".format(self.right)
+
+# class TemperatureSensor:
+#     def __init__(self) -> None:
+#         self.left
+#         self.right
+#     pass   
+# class Potentialmeter:
+#     def __init__(self) -> None:
+#         self.left
+#         self.right
+#     pass   
+# class Photoresistor:
+#     def __init__(self) -> None:
+#         self.left
+#         self.right
+#     pass    
+
+if __name__ == '__main__':
+    print("currently assume one led with button, two pin in board")
+    print("for led assume left is - right is +")
+    print("use default setting with no-malfunctining componet")
+    board = Board()
+    led_right = input("LED right 1 or 2: ")
+    if led_right == "1":
+        led = LED(board, board.pin1)
+        board.gnd.append(led.left)
+        board.pin1.connect.append(led)
+    else:
+        led = LED(board, board.pin2)
+        board.gnd.append(led.left)
+        board.pin2.connect.append(led)
+    btn_right = input("BTN right 1 or 2: ")
+    if btn_right == "1":
+        btn = Button(board.gnd, board.pin1)
+        board.gnd.append(btn.left)
+        board.pin1.connect.append(btn)
+    else:
+        btn = Button(board.gnd, board.pin2)
+        board.gnd.append(btn.left)
+        board.pin2.connect.append(btn)
+    board.code[0] = btn.unpress
+    board.code[1] = btn.press
+    
+    print(board.pin1, board.pin2, board.gnd, board.vcc)
+    print(led, btn)
+    
