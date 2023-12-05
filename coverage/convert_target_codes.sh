531cea6389711b01e2749aea02f732cc3b7694a6
Joohee Kim <joohee.kim@ringleplus.com>
2023-12-05 17:06:15 +0900

    resolve conflict

---
+++ coverage/convert_target_codes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 coverage/convert_target_code.py target_codes/Button.py
+python3 coverage/convert_target_code.py target_codes/IfStatementConditional.py
+python3 coverage/convert_target_code.py target_codes/LoveOMeter.py
+python3 coverage/convert_target_code.py target_codes/StateChangeDetection.py
+python3 coverage/convert_target_code.py target_codes/SwitchCase.py
+python3 coverage/convert_target_code.py target_codes/SegmentDisplay.py
+
+echo "Done converting target codes!"
