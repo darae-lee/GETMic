e37492438df4bdff34b832bd09cc1176ebe36fb9
darea-lee <2darae@kaist.ac.kr>
2023-12-18 01:27:32 +0900

    clean codes

--- coverage/convert_target_codes.sh
+++ coverage/convert_target_codes.sh
@@ -7,7 +7,6 @@ python coverage/convert_target_code.py target_codes/StateChangeDetection.py
 python coverage/convert_target_code.py target_codes/SwitchCase.py
 python coverage/convert_target_code.py target_codes/SegmentDisplay.py
 python coverage/convert_target_code.py target_codes/WarmButton.py
-python coverage/convert_target_code.py target_codes/ButtonCounter.py
 python coverage/convert_target_code.py target_codes/ComplexButton.py
 
 echo "Done converting target codes!"
